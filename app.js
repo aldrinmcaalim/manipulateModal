ebecf605d1fd1e033ea2bd1714551d9573d92dc6
Aldrin <aldrinmcaalim@gmail.com>
2021-03-30 14:54:09 -0400

    Refactored the code
    Created functions to practice making code easier to debug by separating each action with functions

--- app.js
+++ app.js
@@ -5,21 +5,18 @@ const closeModalButton = document.querySelector("#close-modal-btn");
 const overlay = document.querySelector("#overlay");
 const modal = document.querySelector("#modal");
 
-openModalButton.addEventListener("click", () => {
+openModalButton.addEventListener("click", openModal);
+
+closeModalButton.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+function closeModal() {
+  overlay.classList.remove("open");
+  modal.classList.remove("open");
+}
+
+function openModal() {
   modal.classList.add("open");
   overlay.classList.add("open");
-});
-
-closeModalButton.addEventListener("click", () => {
-  modal.classList.remove("open");
-  overlay.classList.remove("open");
-});
-
-overlay.addEventListener("click", () => {
-  overlay.classList.remove("open");
-  modal.classList.remove("open");
-});
-
-function addOpen(element) {
-  element.classList.add("open");
 }
